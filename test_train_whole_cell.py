7317edd464564281121e6c73030b645d58ec0fed
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2023-01-20 00:50:15 -0600

    Got working better

--- test_train_whole_cell.py
+++ test_train_whole_cell.py
@@ -28,7 +28,7 @@ for i, start in enumerate(starts):
         u = system(x)
         du = torch.max(torch.abs(h*u))
         if du < eps:
-            print('converged')
+            print('converged', flush=True)
             print(x)
             runs.append(run[:j])
             break
@@ -46,12 +46,13 @@ for run in runs:
 from matplotlib import pyplot as plt
 for i in range(nstarts):
     plt.scatter(runs[i][:,0], runs[i][:,1], s=1)
-# Add noise to the sampled points
+# TODO Add noise to the sampled points
+# TODO maybe add noise during the simulation
 noise_factor = 0.1
 noisy = sampled + torch.randn(sampled.shape)*noise_factor
 
 #%%
-plt.scatter(sampled[:,0], sampled[:,1], s=1)
+#plt.scatter(sampled[:,0], sampled[:,1], s=1)
 
 
 #%% 
@@ -74,12 +75,11 @@ end = runs[0][-1].detach().numpy()
 adata.uns['iroot'] = np.flatnonzero(np.abs(adata.X - end)<1e-5)[0]
 print('Computing diffmap')
 diffmap(adata)
-print('Computing DPT')
+print('Computing diffusion pseudotime')
 dpt(adata)
 
 
 #%%
-# TODO this generating weird values for the neighbors 
 # Get the nearest neighbors that have a larger pseudo-time for each point
 transition_points = []
 for i in range(len(sampled)):
@@ -90,10 +90,17 @@ for i in range(len(sampled)):
     neighbor_pseudotimes = adata.obs['dpt_pseudotime'][nonzeros]
     current_pseudotime = adata.obs['dpt_pseudotime'][i]
     larger_pseudotimes = neighbor_pseudotimes < current_pseudotime
-    if sum(larger_pseudotimes) == 0:
+    # Check if both points are in the same cluster
+    same_cluster = adata.obs['leiden'][i] == adata.obs['leiden'][nonzeros[larger_pseudotimes]]
+    # Check if their clusters are connected in the paga graph
+    # TODO TODO TODO
+    idxs = np.flatnonzero(same_cluster & larger_pseudotimes)
+
+    if sum(idxs) == 0:
         transition_points.append(sampled[i])
     else:
-        next_points = nonzeros[larger_pseudotimes]
+        next_points = nonzeros[idxs]
+        mean_transition_point = torch.mean(sampled[next_points], dim=0)
         min_dist_nonzero_idx = adata.obsp['distances'][i, next_points].argmin()
         min_dist_idx = next_points[min_dist_nonzero_idx]
         min_dist_point = sampled[min_dist_idx]
@@ -103,29 +110,37 @@ min_transition_points = torch.vstack(transition_points)
 # mean_transition_points = torch.vstack([torch.mean(sampled[points], dim=0) for points in transition_points])
 
 #%%
-# Plot the vectors from the sampled points to the transition points
-d = min_transition_points - sampled
-# Increase the size of the plot to see the vectors
-plt.figure(figsize=(30,30))
-# Plot the data with the pseudo-time as color
-plt.scatter(adata.X[:,0], adata.X[:,1], c=adata.obs['dpt_pseudotime'], s=5)
-
-for i in range(0, len(d), 10):
-    plt.arrow(sampled[i,0], sampled[i,1], d[i,0], d[i,1], color='r', alpha=1)
+def plot_arrows(start_points, next_points, sample_factor=10, save_file=None):
+    # Plot the vectors from the sampled points to the transition points
+    d = next_points - start_points
+    # Increase the size of the plot to see the vectors
+    plt.figure(figsize=(15,15))
+    # Plot the data with the pseudo-time as color
+    # TODO: depends on global context
+    #plt.scatter(adata.X[:,0], adata.X[:,1], c=adata.obs['dpt_pseudotime'], s=5)
     
+    for i in range(0, len(d), sample_factor):
+        plt.arrow(start_points[i,0], start_points[i,1], d[i,0], d[i,1], color='r', alpha=1)
+
+    if save_file is not None:
+        plt.savefig(save_file)
+        plt.close()
+
+plot_arrows(sampled.detach().cpu(), min_transition_points.detach().cpu())
 
 #%%
+device = 'cuda:0'
 model = WholeCell(input_dim=num_nodes, 
                   output_dim=num_nodes, 
-                  hidden_dim=64, num_layers=3)
+                  hidden_dim=12, num_layers=3).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 loss_fn = torch.nn.MSELoss(reduction='mean')
-  
-
+sampled = sampled.to(device)
+min_transition_points = min_transition_points.to(device)
 #%%
 mse = torch.nn.MSELoss(reduction='mean')
 
-n_epoch = 1000
+n_epoch = 10000
 n_points = 1000
 n_traces = 50
 n_samples = 10
@@ -148,11 +163,22 @@ for i in range(n_epoch):
     # loss = mse(fx, min_next_points)
     loss.backward()
     optimizer.step()
-    
+    print(i,' '.join([f'{x.item():.9f}' for x in 
+          [loss]]), flush=True)
+    if i % 100 == 0:
+        plot_arrows(sampled.detach().cpu(), 
+                    fx[-1].detach().cpu(),
+                    sample_factor=10,
+                    save_file=f'figures/test/vector_field_{i}.png')
+    if i%1000 == 0:
+        plot_traces(model, sampled, i, n_traces=100)
+
+#%%
+def plot_traces(model, sampled, i, n_traces=50):
     # Generate some sample traces
     trace_starts = sampled[torch.randint(0, sampled.shape[0], (n_traces,))]
-    traces = model.trajectory(state=trace_starts, tspan=torch.linspace(0, 100, 100))
-    trace_plot = traces.detach().numpy()
+    traces = model.trajectory(state=trace_starts, tspan=torch.linspace(0, 100, 500))
+    trace_plot = traces.cpu().detach().numpy()
     # Create a new figure
     fig, ax = plt.subplots()
     # Plot the data with partial transparency so that we can highlight the traces
@@ -161,11 +187,10 @@ for i in range(n_epoch):
         # Plot the traces
         ax.scatter(trace_plot[:,trace,0], trace_plot[:,trace,1], s=1)
     # Save the plot to a file indicating the epoch
-    plt.savefig(f'figures/test/epoch_{i}.png')
+    plt.savefig(f'figures/test/traces_{i}.png')
     plt.close()
 
-    print(i,' '.join([f'{x.item():.9f}' for x in 
-         [loss]]))
+
 
 torch.save(model.state_dict(), 'simple_model.torch')
 # %%
